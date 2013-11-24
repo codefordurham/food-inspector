d1da571ebc6d43e9e9b1a99aeda770bcf38fb30a
Tyler Pearson <ty.pearson@gmail.com>
2013-11-23 19:16:07 -0500

    Switch to GeoJSON for only food locations

--- scripts/map.js
+++ scripts/map.js
@@ -8,17 +8,26 @@
             lat: 35.9886,
             lng: -78.9072
         },
-        locationLayerPath = "/data/raw.json",
+        locationLayerPath = "/data/food-locations.geojson",
         createMap = function () {
+            var markerLayer = L.mapbox.markerLayer();
+
             // setup a Code for Durham MapBox account for map?
             map = L.mapbox.map('map', 'tylerpearson.gc56ggok', {zoomControl: false})
-                      .setView([userLocation.lat, userLocation.lng], 17);
+                      .setView([userLocation.lat, userLocation.lng], 18);
 
             new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
 
-            var locationLayer = L.mapbox.markerLayer()
-                .loadURL(locationLayerPath)
+
+            markerLayer.loadURL(locationLayerPath)
                 .addTo(map);
+
+            markerLayer.eachLayer(function (layer) {
+                var content = '<h1>size: ' + layer.feature.properties.size + '<\/h1>' +
+                        '<h2>population: ' + layer.feature.properties.population + '<\/h2>';
+
+                layer.bindPopup(content);
+            });
 
         },
         getUserLocation = function (callbackFunc) {
