01e23c7e273f89eac988d0f2a682870850c7ec44
Tyler Pearson <ty.pearson@gmail.com>
2013-11-24 02:22:37 -0500

    Add marker with user's detected location

--- scripts/map.js
+++ scripts/map.js
@@ -9,28 +9,36 @@
             lng: -78.9072
         },
         locationLayerPath = "data/food-locations.geojson",
-        initMap = function () {
-            var markerLayer = L.mapbox.markerLayer();
+        addMarker = function (lat, lng, markerHexColor, markerSymbol, markerSize) {
+            var markerInfo = {
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [lng, lat]
+                    },
+                    properties: {}
+                };
 
-            // setup a Code for Durham MapBox account for map?
-            map = L.mapbox.map('map', 'tylerpearson.gc56ggok', {zoomControl: false})
-                      .setView([userLocation.lat, userLocation.lng], 17);
+            // clean this up at some point
+            if (markerHexColor !== undefined) {
+                markerInfo.properties['marker-color'] = markerHexColor;
+            }
 
-            new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
+            if (markerSymbol !== undefined) {
+                markerInfo.properties['marker-symbol'] = markerSymbol;
+            }
 
+            if (markerSize !== undefined) {
+                markerInfo.properties['marker-size'] = markerSize;
+            }
 
-            markerLayer.loadURL(locationLayerPath)
-                .addTo(map);
-
-            map.markerLayer.on('click', function(e) {
-                    map.panTo(e.layer.getLatLng());
-                });
-
+            L.mapbox.markerLayer(markerInfo).addTo(map);
         },
         getUserLocation = function (callbackFunc) {
             var success = function (position) {
                     userLocation.lat = position.coords.latitude;
                     userLocation.lng = position.coords.longitude;
+
                     if (callbackFunc !== undefined) {
                         callbackFunc();
                     }
@@ -46,9 +54,30 @@
                 alert("Please access this site with a browser that supports geolocation");
             }
         },
+        initMap = function () {
+            var markerLayer = L.mapbox.markerLayer();
+
+            // setup a Code for Durham MapBox account for map?
+            map = L.mapbox.map('map', 'tylerpearson.gc56ggok', {zoomControl: false})
+                      .setView([userLocation.lat, userLocation.lng], 17);
+
+            new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
+
+            markerLayer.loadURL(locationLayerPath)
+                .addTo(map);
+
+            map.markerLayer.on('click', function (e) {
+                map.panTo(e.layer.getLatLng());
+            });
+
+        },
         init = function () {
             getUserLocation(function () {
+                // create map
                 initMap();
+
+                // add a marker for the user's detected location
+                addMarker(userLocation.lat, userLocation.lng, '#41b649', 'star', 'large');
             });
         };
 
