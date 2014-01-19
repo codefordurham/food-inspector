7af5fcffe29791bf0d3e82f0a543c1bf80b2c1f3
Colin Copeland <copelco@caktusgroup.com>
2014-01-19 17:04:58 -0500

    allow filtering on est_type and odering by update_date

--- inspections/api/resources.py
+++ inspections/api/resources.py
@@ -1,4 +1,4 @@
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 from tastypie.contrib.gis.resources import ModelResource as GisModelResource
 
 from inspections.models import Establishment
@@ -6,7 +6,13 @@ from inspections.api.serializers import GeoJSONSerializer
 
 
 class EstablishmentResource(GisModelResource):
+
     class Meta:
         queryset = Establishment.objects.all()
         allowed_methods = ['get']
         serializer = GeoJSONSerializer()
+        limit = 40
+        filtering = {
+            'est_type': ALL,
+        }
+        ordering = ['update_date']
