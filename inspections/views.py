d6f977055afe89beb27db464f448660a0432107d
Colin Copeland <copelco@caktusgroup.com>
2014-03-09 15:47:43 -0400

    make list searchable

--- inspections/views.py
+++ inspections/views.py
@@ -1,17 +1,20 @@
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import ListView, DetailView
 from inspections.models import Establishment
 
 
-class EstablishmentList(TemplateView):
+class EstablishmentList(ListView):
+    model = Establishment
+    context_object_name = 'establishments'
     template_name = 'inspections/establishment_list.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(EstablishmentList, self).get_context_data(**kwargs)
-        establishments = Establishment.objects.filter(status='ACTIVE',
-                                                      est_type=1)
-        establishments = establishments.order_by('premise_name')
-        context['establishments'] = establishments[:20]
-        return context
+    def get_queryset(self):
+        query = self.request.GET.get('q', '')
+        objects = self.model.objects.all()
+        if query:
+            objects = objects.filter(premise_name__icontains=query)
+        objects = objects.filter(status='ACTIVE', est_type=1)
+        objects = objects.order_by('premise_name')
+        return objects
 
 
 class EstablishmentDetail(DetailView):
