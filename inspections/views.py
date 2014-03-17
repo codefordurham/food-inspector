b613629f052653a8f2d9487625857ad5aa6193e0
Eric Millin <etmillin@gmail.com>
2014-03-17 04:55:29 -0400

    Issue 32 - Added accordian and basic styling for dumps of raw inspection data

--- inspections/views.py
+++ inspections/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from inspections.models import Establishment
+from inspections.models import Establishment, Inspection
 
 
 class EstablishmentList(ListView):
@@ -28,8 +28,34 @@ class EstablishmentDetail(DetailView):
     model = Establishment
     context_object_name = 'establishment'
 
+
+    @staticmethod
+    def dump_inspection_to_dict(inspection):
+        """
+        Convenience function that dumps key-value-paired inspection data into a dictionary.
+        """
+        assert isinstance(inspection, Inspection)
+
+        dump = dict()
+
+        for attr in dir(inspection):
+            try:
+                if not callable(getattr(inspection, attr)) and not attr.startswith('_'):
+                    # at some point we'll want to get localized string for this attribute
+                    dump[attr] = getattr(inspection, attr)
+            except AttributeError:
+                pass
+
+        return dump
+
+
     def get_context_data(self, **kwargs):
         context = super(EstablishmentDetail, self).get_context_data(**kwargs)
         establishment = context['establishment']
         context['inspections'] = establishment.inspections.order_by('-insp_date')
+
+        # get a dump to populate the details table for each inspection
+        for inspection in context['inspections']:
+            inspection.dump = EstablishmentDetail.dump_inspection_to_dict(inspection)
+
         return context
