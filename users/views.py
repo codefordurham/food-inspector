a662de68d3b8dc84f7dd4bd0b00e21382abfb435
Victor Rocha <victor@rochapps.com>
2014-03-23 15:22:13 -0400

    Adds logging information for the add location view.

--- users/views.py
+++ users/views.py
@@ -1,21 +1,27 @@
 import json
+import logging
 
 from django.http import HttpResponse
 from django.views.generic import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
+logger = logging.getLogger(__name__)
+
 
 class CSRFExemptMixin(object):
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
+        logger.debug("CSRFExemptMixin: dispatch method")
         return super(CSRFExemptMixin, self).dispatch(*args, **kwargs)
 
 
 class UserAddLocationView(CSRFExemptMixin, View):
     def post(self, request, *args, **kwargs):
+        logger.debug(request.POST)
         lat = request.POST.get('lat', '')
         lon = request.POST.get('lon', '')
+        logger.debug("Latitude: {0} and longitude: {1}".format(lat, lon))
         # adds the lation object into the session
         request.session['location'] = {'lat': lat, 'lon': lon}
         data = json.dumps({'status': 'success'})
