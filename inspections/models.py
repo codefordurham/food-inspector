fc5b885dec367c28c37abe0edf740e9d7746ab22
Victor Rocha <victor@rochapps.com>
2014-04-24 22:28:03 -0400

    [refs #72] Mark model fields for translation

--- inspections/models.py
+++ inspections/models.py
@@ -2,29 +2,32 @@
 # v1.0 - http://www.yelp.com/healthscores
 
 from django.contrib.gis.db import models
+from django.utils.translation import ugettext as _
 
 
 class Establishment(models.Model):
     """Business or restaurant property"""
 
-    STATUS_CHOICES = (('deleted', 'Deleted'), ('active', 'Active'))
+    STATUS_CHOICES = (('deleted', _('Deleted')), ('active', _('Active')))
 
-    external_id = models.CharField("External ID", max_length=128)
-    state_id = models.BigIntegerField("State ID")
-    property_id = models.CharField("Property ID", max_length=128, blank=True)
-    name = models.CharField(max_length=255)
-    type = models.PositiveIntegerField(default=0)
-    address = models.CharField(max_length=255)
-    city = models.CharField(max_length=64)
-    county = models.CharField(max_length=64, db_index=True)
-    state = models.CharField(max_length=64)
-    postal_code = models.CharField(max_length=16)
-    phone_number = models.CharField(max_length=64, blank=True)
-    opening_date = models.DateTimeField()
-    update_date = models.DateTimeField(null=True, blank=True, db_index=True)
-    status = models.CharField(choices=STATUS_CHOICES, max_length=32,
+    external_id = models.CharField(_("External ID"), max_length=128)
+    state_id = models.BigIntegerField(_("State ID"))
+    property_id = models.CharField(_("Property ID"), max_length=128, blank=True)
+    name = models.CharField(_("name"), max_length=255)
+    type = models.PositiveIntegerField(_("type"), default=0)
+    address = models.CharField(_("address"), max_length=255)
+    city = models.CharField(_("city"), max_length=64)
+    county = models.CharField(_("county"), max_length=64, db_index=True)
+    state = models.CharField(_("state"), max_length=64)
+    postal_code = models.CharField(_("postal code"), max_length=16)
+    phone_number = models.CharField(_("phone number"), max_length=64,
+                                    blank=True)
+    opening_date = models.DateTimeField(_("opening date"))
+    update_date = models.DateTimeField(_("update date"), null=True, blank=True,
+                                       db_index=True)
+    status = models.CharField(_("status"), choices=STATUS_CHOICES, max_length=32,
                               default='active')
-    location = models.PointField(null=True, blank=True)
+    location = models.PointField(_("location"), null=True, blank=True)
 
     objects = models.GeoManager()
 
@@ -54,13 +57,16 @@ class Inspection(models.Model):
     )
 
     establishment = models.ForeignKey(Establishment,
+                                      verbose_name=_("establishment"),
                                       related_name='inspections')
-    external_id = models.CharField("External ID", max_length=128)
-    date = models.DateTimeField(db_index=True)
-    score = models.FloatField(null=True, blank=True)
-    description = models.TextField(blank=True)
-    type = models.PositiveIntegerField(default=0, choices=TYPE_CHOICES)
-    update_date = models.DateTimeField(null=True, blank=True, db_index=True)
+    external_id = models.CharField(_("External ID"), max_length=128)
+    date = models.DateTimeField(_("date"), db_index=True)
+    score = models.FloatField(_("score"), null=True, blank=True)
+    description = models.TextField(_("description"), blank=True)
+    type = models.PositiveIntegerField(_("type"), default=0,
+                                       choices=TYPE_CHOICES)
+    update_date = models.DateTimeField(_("update date"), null=True, blank=True,
+                                       db_index=True)
 
     def __str__(self):
         return "Inspection #{}".format(self.pk)
@@ -70,11 +76,14 @@ class Violation(models.Model):
     """Information about specific inspection violations"""
 
     establishment = models.ForeignKey(Establishment,
+                                      verbose_name=_("establishment"),
                                       related_name='violations')
     inspection = models.ForeignKey(Inspection, related_name='violations',
-                                   null=True, blank=True)
-    external_id = models.CharField("External ID", max_length=128)
-    date = models.DateTimeField(db_index=True)
-    code = models.CharField(max_length=32)
-    description = models.TextField(blank=True)
-    update_date = models.DateTimeField(null=True, blank=True, db_index=True)
+                                   verbose_name=_("inspection"), null=True,
+                                   blank=True)
+    external_id = models.CharField(_("External ID"), max_length=128)
+    date = models.DateTimeField(_("date"), db_index=True)
+    code = models.CharField(_("date"), max_length=32)
+    description = models.TextField(_("description"), blank=True)
+    update_date = models.DateTimeField(_("update date"), null=True, blank=True,
+                                       db_index=True)
