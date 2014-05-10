20668d7afb80c6a9110e9a40d522dc128c39905f
Victor Rocha <victor@rochapps.com>
2014-05-10 13:43:16 -0400

    [refs #72] fixes translations not showing in the admin

--- inspections/models.py
+++ inspections/models.py
@@ -2,69 +2,69 @@
 # v1.0 - http://www.yelp.com/healthscores
 
 from django.contrib.gis.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy
 
 
 class Establishment(models.Model):
     """Business or restaurant property"""
-    STATUS_CHOICES = (('deleted', _('Deleted')), ('active', _('Active')))
+    STATUS_CHOICES = (('deleted', ugettext_lazy('Deleted')), ('active', ugettext_lazy('Active')))
     TYPE_CHOICES = (
-        (0, _('Unknown')),
-        (1, _('Restaurant')),
-        (2, _('Food Stand')),
-        (3, _('Mobile Food')),
-        (4, _('Push Cart')),
-        (5, _('Private School\'s Cafeteria')),
-        (6, _('Educational Food Service')),
-        (9, _('Elderly Nutrition')),
-        (11, _('Public School\'s Cafeteria')),
-        (12, _('Elderly Nutrition')),
-        (14, _('Limited Food')),
-        (15, _('Commissary (Pushcarts/Mobile Food),')),
-        (16, _('Institutional Food Service')),
-        (20, _('Lodging')),
-        (21, _('Bed & Breakfast Home')),
-        (22, _('Summer Camp')),
-        (23, _('Bed & Breakfast Inn')),
-        (25, _('Primitive Experience Camp')),
-        (26, _('Resident Camp')),
-        (30, _('Meat Market')),
-        (40, _('Rest/Nursing Home')),
-        (41, _('Hospital')),
-        (42, _('Child Care')),
-        (43, _('Residential Care')),
-        (44, _('School Building')),
-        (45, _('Local Confinement')),
-        (46, _('Private Boarding School/College')),
-        (47, _('Orphanage, Children\'s Home')),
-        (48, _('Adult Day Care')),
-        (49, _('Adult Day Service')),
-        (50, _('Seasonal Swimming Pool')),
-        (51, _('Seasonal Wading Pool')),
-        (52, _('Seasonal Spa')),
-        (53, _('Year-Round Swimming Pool')),
-        (54, _('Year-Round Wading Pool')),
-        (55, _('Year-Round Spa')),
-        (61, _('Tattoo Artist')),
-        (72, _('Summer Feeding Program')),
-        (73, _('Temporary Food Establishment')),
+        (0, ugettext_lazy('Unknown')),
+        (1, ugettext_lazy('Restaurant')),
+        (2, ugettext_lazy('Food Stand')),
+        (3, ugettext_lazy('Mobile Food')),
+        (4, ugettext_lazy('Push Cart')),
+        (5, ugettext_lazy('Private School\'s Cafeteria')),
+        (6, ugettext_lazy('Educational Food Service')),
+        (9, ugettext_lazy('Elderly Nutrition')),
+        (11, ugettext_lazy('Public School\'s Cafeteria')),
+        (12, ugettext_lazy('Elderly Nutrition')),
+        (14, ugettext_lazy('Limited Food')),
+        (15, ugettext_lazy('Commissary (Pushcarts/Mobile Food),')),
+        (16, ugettext_lazy('Institutional Food Service')),
+        (20, ugettext_lazy('Lodging')),
+        (21, ugettext_lazy('Bed & Breakfast Home')),
+        (22, ugettext_lazy('Summer Camp')),
+        (23, ugettext_lazy('Bed & Breakfast Inn')),
+        (25, ugettext_lazy('Primitive Experience Camp')),
+        (26, ugettext_lazy('Resident Camp')),
+        (30, ugettext_lazy('Meat Market')),
+        (40, ugettext_lazy('Rest/Nursing Home')),
+        (41, ugettext_lazy('Hospital')),
+        (42, ugettext_lazy('Child Care')),
+        (43, ugettext_lazy('Residential Care')),
+        (44, ugettext_lazy('School Building')),
+        (45, ugettext_lazy('Local Confinement')),
+        (46, ugettext_lazy('Private Boarding School/College')),
+        (47, ugettext_lazy('Orphanage, Children\'s Home')),
+        (48, ugettext_lazy('Adult Day Care')),
+        (49, ugettext_lazy('Adult Day Service')),
+        (50, ugettext_lazy('Seasonal Swimming Pool')),
+        (51, ugettext_lazy('Seasonal Wading Pool')),
+        (52, ugettext_lazy('Seasonal Spa')),
+        (53, ugettext_lazy('Year-Round Swimming Pool')),
+        (54, ugettext_lazy('Year-Round Wading Pool')),
+        (55, ugettext_lazy('Year-Round Spa')),
+        (61, ugettext_lazy('Tattoo Artist')),
+        (72, ugettext_lazy('Summer Feeding Program')),
+        (73, ugettext_lazy('Temporary Food Establishment')),
     )
-    external_id = models.CharField(_("External ID"), max_length=128)
-    state_id = models.BigIntegerField(_("State ID"))
-    property_id = models.CharField(_("Property ID"), max_length=128, blank=True)
-    name = models.CharField(_("Name"), max_length=255)
-    type = models.PositiveIntegerField(_("Type"), default=0, choices=TYPE_CHOICES)
-    address = models.CharField(_("Address"), max_length=255)
-    city = models.CharField(_("City"), max_length=64)
-    county = models.CharField(_("County"), max_length=64, db_index=True)
-    state = models.CharField(_("State"), max_length=64)
-    postal_code = models.CharField(_("Postal Code"), max_length=16)
-    phone_number = models.CharField(_("Phone Number"), max_length=64, blank=True)
-    opening_date = models.DateTimeField(_("Opening Date"))
-    update_date = models.DateTimeField(_("Update Date"), null=True, blank=True, db_index=True)
-    status = models.CharField(_("Status"), choices=STATUS_CHOICES, max_length=32,
+    external_id = models.CharField(ugettext_lazy("External ID"), max_length=128)
+    state_id = models.BigIntegerField(ugettext_lazy("State ID"))
+    property_id = models.CharField(ugettext_lazy("Property ID"), max_length=128, blank=True)
+    name = models.CharField(ugettext_lazy("Name"), max_length=255)
+    type = models.PositiveIntegerField(ugettext_lazy("Type"), default=0, choices=TYPE_CHOICES)
+    address = models.CharField(ugettext_lazy("Address"), max_length=255)
+    city = models.CharField(ugettext_lazy("City"), max_length=64)
+    county = models.CharField(ugettext_lazy("County"), max_length=64, db_index=True)
+    state = models.CharField(ugettext_lazy("State"), max_length=64)
+    postal_code = models.CharField(ugettext_lazy("Postal Code"), max_length=16)
+    phone_number = models.CharField(ugettext_lazy("Phone Number"), max_length=64, blank=True)
+    opening_date = models.DateTimeField(ugettext_lazy("Opening Date"))
+    update_date = models.DateTimeField(ugettext_lazy("Update Date"), null=True, blank=True, db_index=True)
+    status = models.CharField(ugettext_lazy("Status"), choices=STATUS_CHOICES, max_length=32,
                               default='active')
-    location = models.PointField(_("location"), null=True, blank=True)
+    location = models.PointField(ugettext_lazy("location"), null=True, blank=True)
 
     objects = models.GeoManager()
 
@@ -79,30 +79,30 @@ class Inspection(models.Model):
     """Information about inspectors' visits to establishments"""
 
     TYPE_CHOICES = (
-        (0, _('Unknown')),
-        (1, _('Routine Inspection')),
-        (2, _('Re-inspection')),
-        (5, _('Permit')),
-        (6, _('Visit')),
-        (8, _('Name Change')),
-        (9, _('Verification')),
-        (10, _('Other')),
-        (12, _('Status Change')),
-        (13, _('Pre-opening Visit')),
-        (31, _('Critical Violation Visit')),
-        (32, _('Critical Violation Followup')),
+        (0, ugettext_lazy('Unknown')),
+        (1, ugettext_lazy('Routine Inspection')),
+        (2, ugettext_lazy('Re-inspection')),
+        (5, ugettext_lazy('Permit')),
+        (6, ugettext_lazy('Visit')),
+        (8, ugettext_lazy('Name Change')),
+        (9, ugettext_lazy('Verification')),
+        (10, ugettext_lazy('Other')),
+        (12, ugettext_lazy('Status Change')),
+        (13, ugettext_lazy('Pre-opening Visit')),
+        (31, ugettext_lazy('Critical Violation Visit')),
+        (32, ugettext_lazy('Critical Violation Followup')),
     )
 
     establishment = models.ForeignKey(Establishment,
-                                      verbose_name=_("Establishment"),
+                                      verbose_name=ugettext_lazy("Establishment"),
                                       related_name='inspections')
-    external_id = models.CharField(_("External ID"), max_length=128)
-    date = models.DateTimeField(_("Date"), db_index=True)
-    score = models.FloatField(_("Score"), null=True, blank=True)
-    description = models.TextField(_("Description"), blank=True)
-    type = models.PositiveIntegerField(_("Type"), default=0,
+    external_id = models.CharField(ugettext_lazy("External ID"), max_length=128)
+    date = models.DateTimeField(ugettext_lazy("Date"), db_index=True)
+    score = models.FloatField(ugettext_lazy("Score"), null=True, blank=True)
+    description = models.TextField(ugettext_lazy("Description"), blank=True)
+    type = models.PositiveIntegerField(ugettext_lazy("Type"), default=0,
                                        choices=TYPE_CHOICES)
-    update_date = models.DateTimeField(_("Update Date"), null=True, blank=True,
+    update_date = models.DateTimeField(ugettext_lazy("Update Date"), null=True, blank=True,
                                        db_index=True)
 
     def __str__(self):
@@ -113,14 +113,14 @@ class Violation(models.Model):
     """Information about specific inspection violations"""
 
     establishment = models.ForeignKey(Establishment,
-                                      verbose_name=_("Establishment"),
+                                      verbose_name=ugettext_lazy("Establishment"),
                                       related_name='violations')
     inspection = models.ForeignKey(Inspection, related_name='violations',
-                                   verbose_name=_("Inspection"), null=True,
+                                   verbose_name=ugettext_lazy("Inspection"), null=True,
                                    blank=True)
-    external_id = models.CharField(_("External ID"), max_length=128)
-    date = models.DateTimeField(_("Date"), db_index=True)
-    code = models.CharField(_("Code"), max_length=32)
-    description = models.TextField(_("Description"), blank=True)
-    update_date = models.DateTimeField(_("Update Date"), null=True, blank=True,
+    external_id = models.CharField(ugettext_lazy("External ID"), max_length=128)
+    date = models.DateTimeField(ugettext_lazy("Date"), db_index=True)
+    code = models.CharField(ugettext_lazy("Code"), max_length=32)
+    description = models.TextField(ugettext_lazy("Description"), blank=True)
+    update_date = models.DateTimeField(ugettext_lazy("Update Date"), null=True, blank=True,
                                        db_index=True)
