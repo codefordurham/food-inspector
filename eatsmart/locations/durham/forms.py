7530511d5cf9400d72c407c107fcc8dac9ca0ab1
Marek Laska <git@tamcap.one.pl>
2014-05-16 03:20:49 -0400

    Form code to clean imported fields
    * Empty phone number used to be represented by 0. Now it's NULL.
    * Make the city field title-case at import

--- eatsmart/locations/durham/forms.py
+++ eatsmart/locations/durham/forms.py
@@ -37,6 +37,10 @@ class EstablishmentForm(forms.ModelForm):
         model = Establishment
         exclude = ('location',)
 
+    def clean_city(self):
+        city = self.cleaned_data['city']
+        return city.title()
+
     def clean_status(self):
         status = self.cleaned_data['status']
         if status == 'ACTIVE':
@@ -48,7 +52,7 @@ class EstablishmentForm(forms.ModelForm):
     def clean_phone(self):
         phone = self.cleaned_data['phone']
         # Force empty phone value to empty string 
-        if phone == '0':
+        if phone == 'NULL':
             phone = ''
         return phone 
 
