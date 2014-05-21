e5f3806963fca5ff396a9c910505729a53da0e3e
Marek Laska <git@tamcap.one.pl>
2014-05-21 01:51:41 -0400

    Pep8 fixes.

--- inspections/models.py
+++ inspections/models.py
@@ -7,7 +7,8 @@ from django.utils.translation import ugettext_lazy
 
 class Establishment(models.Model):
     """Business or restaurant property"""
-    STATUS_CHOICES = (('deleted', ugettext_lazy('Deleted')), ('active', ugettext_lazy('Active')))
+    STATUS_CHOICES = (('deleted', ugettext_lazy('Deleted')),
+                      ('active', ugettext_lazy('Active')))
     TYPE_CHOICES = (
         (0, ugettext_lazy('Unknown')),
         (1, ugettext_lazy('Restaurant')),
@@ -49,23 +50,31 @@ class Establishment(models.Model):
         (72, ugettext_lazy('Summer Feeding Program')),
         (73, ugettext_lazy('Temporary Food Establishment')),
     )
-    external_id = models.CharField(ugettext_lazy("External ID"), max_length=128)
+    external_id = models.CharField(ugettext_lazy("External ID"),
+                                   max_length=128)
     state_id = models.BigIntegerField(ugettext_lazy("State ID"))
-    property_id = models.CharField(ugettext_lazy("Property ID"), max_length=128, blank=True)
+    property_id = models.CharField(ugettext_lazy("Property ID"),
+                                   max_length=128, blank=True)
     name = models.CharField(ugettext_lazy("Name"), max_length=255)
-    pretty_name = models.CharField(ugettext_lazy("Pretty Name"), max_length=255, blank=True)
-    type = models.PositiveIntegerField(ugettext_lazy("Type"), default=0, choices=TYPE_CHOICES)
+    pretty_name = models.CharField(ugettext_lazy("Pretty Name"),
+                                   max_length=255, blank=True)
+    type = models.PositiveIntegerField(ugettext_lazy("Type"),
+                                       default=0, choices=TYPE_CHOICES)
     address = models.CharField(ugettext_lazy("Address"), max_length=255)
     city = models.CharField(ugettext_lazy("City"), max_length=64)
-    county = models.CharField(ugettext_lazy("County"), max_length=64, db_index=True)
+    county = models.CharField(ugettext_lazy("County"), max_length=64,
+                              db_index=True)
     state = models.CharField(ugettext_lazy("State"), max_length=64)
     postal_code = models.CharField(ugettext_lazy("Postal Code"), max_length=16)
-    phone_number = models.CharField(ugettext_lazy("Phone Number"), max_length=64, blank=True)
+    phone_number = models.CharField(ugettext_lazy("Phone Number"),
+                                    max_length=64, blank=True)
     opening_date = models.DateTimeField(ugettext_lazy("Opening Date"))
-    update_date = models.DateTimeField(ugettext_lazy("Update Date"), null=True, blank=True, db_index=True)
-    status = models.CharField(ugettext_lazy("Status"), choices=STATUS_CHOICES, max_length=32,
-                              default='active')
-    location = models.PointField(ugettext_lazy("location"), null=True, blank=True)
+    update_date = models.DateTimeField(ugettext_lazy("Update Date"),
+                                       null=True, blank=True, db_index=True)
+    status = models.CharField(ugettext_lazy("Status"), choices=STATUS_CHOICES,
+                              max_length=32, default='active')
+    location = models.PointField(ugettext_lazy("location"),
+                                 null=True, blank=True)
 
     objects = models.GeoManager()
 
@@ -77,20 +86,25 @@ class Establishment(models.Model):
             return self.pretty_name
         return self.name
 
+
 class Inspector(models.Model):
     """Information about inspectors"""
 
     external_id = models.CharField(ugettext_lazy("External ID"), max_length=5)
     name = models.CharField(ugettext_lazy("Name"), max_length=128)
-    phone_number = models.CharField(ugettext_lazy("Phone Number"), max_length=64, blank=True)
-    e_mail = models.CharField(ugettext_lazy("E-mail address"), max_length=128, blank=True)
-    county = models.CharField(ugettext_lazy("County"), max_length=64, db_index=True)
-    
+    phone_number = models.CharField(ugettext_lazy("Phone Number"),
+                                    max_length=64, blank=True)
+    e_mail = models.CharField(ugettext_lazy("E-mail address"), max_length=128,
+                              blank=True)
+    county = models.CharField(ugettext_lazy("County"), max_length=64,
+                              db_index=True)
+
     class Meta(object):
         unique_together = ('external_id', 'county')
 
     def __str__(self):
         return self.name
+
 
 class Inspection(models.Model):
     """Information about inspectors' visits to establishments"""
@@ -111,19 +125,21 @@ class Inspection(models.Model):
     )
 
     establishment = models.ForeignKey(Establishment,
-                                      verbose_name=ugettext_lazy("Establishment"),
+                                      verbose_name=ugettext_lazy(
+                                          "Establishment"),
                                       related_name='inspections')
-    external_id = models.CharField(ugettext_lazy("External ID"), max_length=128)
+    external_id = models.CharField(ugettext_lazy("External ID"),
+                                   max_length=128)
     date = models.DateTimeField(ugettext_lazy("Date"), db_index=True)
     score = models.FloatField(ugettext_lazy("Score"), null=True, blank=True)
     description = models.TextField(ugettext_lazy("Description"), blank=True)
     inspector = models.ForeignKey(Inspector,
-                                     verbose_name=ugettext_lazy("Inspector"),
-                                     related_name='inspections', blank=True)
+                                  verbose_name=ugettext_lazy("Inspector"),
+                                  related_name='inspections', blank=True)
     type = models.PositiveIntegerField(ugettext_lazy("Type"), default=0,
                                        choices=TYPE_CHOICES)
-    update_date = models.DateTimeField(ugettext_lazy("Update Date"), null=True, blank=True,
-                                       db_index=True)
+    update_date = models.DateTimeField(ugettext_lazy("Update Date"), null=True,
+                                       blank=True, db_index=True)
 
     def __str__(self):
         return "Inspection #{}".format(self.pk)
@@ -133,14 +149,16 @@ class Violation(models.Model):
     """Information about specific inspection violations"""
 
     establishment = models.ForeignKey(Establishment,
-                                      verbose_name=ugettext_lazy("Establishment"),
+                                      verbose_name=ugettext_lazy(
+                                          "Establishment"),
                                       related_name='violations')
     inspection = models.ForeignKey(Inspection, related_name='violations',
-                                   verbose_name=ugettext_lazy("Inspection"), null=True,
-                                   blank=True)
-    external_id = models.CharField(ugettext_lazy("External ID"), max_length=128)
+                                   verbose_name=ugettext_lazy("Inspection"),
+                                   null=True, blank=True)
+    external_id = models.CharField(ugettext_lazy("External ID"),
+                                   max_length=128)
     date = models.DateTimeField(ugettext_lazy("Date"), db_index=True)
     code = models.CharField(ugettext_lazy("Code"), max_length=32)
     description = models.TextField(ugettext_lazy("Description"), blank=True)
-    update_date = models.DateTimeField(ugettext_lazy("Update Date"), null=True, blank=True,
-                                       db_index=True)
+    update_date = models.DateTimeField(ugettext_lazy("Update Date"), null=True,
+                                       blank=True, db_index=True)
