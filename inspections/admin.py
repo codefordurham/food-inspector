61b368260cdd1137163db4e8fb3a3c2f8647d32b
Victor Rocha <victor@rochapps.com>
2014-06-17 22:30:40 -0400

    [refs #112] Improves the admin list display for Establishments.

--- inspections/admin.py
+++ inspections/admin.py
@@ -1,19 +1,63 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
+from django.utils.encoding import force_text
+from django.utils.translation import ugettext_lazy as _
+
 from inspections.models import Establishment, Inspection, Violation
 from leaflet.admin import LeafletGeoAdmin
 
 
+class ImageFilter(SimpleListFilter):
+    title = _('Image')
+
+    parameter_name = 'image'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('all', _('All')),
+            ('yes', _('Yes')),
+            ('no', _('No')),
+        )
+
+    def choices(self, cl):
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': (self.value() == force_text(lookup) or
+                             not self.value() and force_text(lookup) == "all"),
+                'query_string': cl.get_query_string({
+                    self.parameter_name: lookup,
+                }, []),
+                'display': title,
+            }
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if not value == 'all':
+            if value == 'yes':
+                return queryset.exclude(image_url='')
+            else:
+                return queryset.filter(image_url='')
+        return queryset
+
+
 class EstablishmentAdmin(LeafletGeoAdmin):
     search_fields = ('name', 'address')
-    list_display = ('id', 'name', 'type',
-                    'county', 'state_id', 'image_url', 'property_id', 'point', 'update_date')
-    list_filter = ('county', 'postal_code')
+    list_display = ('id', 'name', 'type', 'county', 'state_id', 'image',
+                    'point', 'update_date')
+    list_filter = ('county', ImageFilter, 'postal_code')
     ordering = ('-update_date',)
 
     def point(self, obj):
         if obj.location:
             return "{}, {}".format(obj.location[0], obj.location[1])
         return None
+
+    def image(self, obj):
+        if not obj.image_url:
+            return ''
+        return u'<a href="{0}"><img src="/static/admin/img/icon-yes.gif" />' \
+               u'</a>'.format(obj.image_url)
+    image.allow_tags = True
 
 
 class InspectionAdmin(admin.ModelAdmin):
