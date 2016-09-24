37ccbe838b67a2a1e05f50f1aa4251c117718c0f
Victor Rocha <victor@rochapps.com>
2016-09-24 12:39:03 -0400

    WIP mode import stuff

--- eatsmart/locations/wake/management/commands/import_wake.py
+++ eatsmart/locations/wake/management/commands/import_wake.py
@@ -12,40 +12,25 @@ class Command(BaseCommand):
     """Import saniation data from Durham County API"""
     restaurants_url = 'http://data.wake.opendata.arcgis.com/datasets/124c2187da8c41c59bde04fa67eb2872_0.geojson?where=&geometry={"xmin":-8992487.871106919,"ymin":4226590.155355151,"xmax":-8508182.859892275,"ymax":4318314.589297318,"spatialReference":{"wkid":102100}}'
     inspections_url = 'http://data.wake.opendata.arcgis.com/datasets/ebe3ae7f76954fad81411612d7c4fb17_1.geojson'
+    violations_url = 'http://data.wake.opendata.arcgis.com/datasets/ebe3ae7f76954fad81411612d7c4fb17_1.geojson'
 
     def handle(self, *args, **options):
         # restaurants
-        restaurants = self.get_restaurants()
+        restaurants = self.get_county_data(self.restaurants_url)
         self.save_restaurants(restaurants)
         # inspections
-        inspections = self.get_inpsections()
+        inspections = self.get_county_data(self.inspections_url)
 
         # violations
-        violations = self.get_county_data()
+        violations = self.get_county_data(self.violations_url)
 
     def get_county_data(self, url):
-        resp = requests.get('http://data.wake.opendata.arcgis.com/datasets/ebe3ae7f76954fad81411612d7c4fb17_1.geojson')
+        resp = requests.get(url)
         json_resp = resp.json()
         if 'features' in json_resp:
             return json_resp['features']
         time.sleep(30)  # wake queues up the response
-        return self.get_restaurants()
-
-    def get_restaurants(self):
-        resp = requests.get('http://data.wake.opendata.arcgis.com/datasets/124c2187da8c41c59bde04fa67eb2872_0.geojson?where=&geometry={"xmin":-8992487.871106919,"ymin":4226590.155355151,"xmax":-8508182.859892275,"ymax":4318314.589297318,"spatialReference":{"wkid":102100}}')
-        json_resp = resp.json()
-        if 'features' in json_resp:
-            return json_resp['features']
-        time.sleep(30)  # wake queues up the response
-        return self.get_restaurants()
-
-    def get_inpsections(self):
-        resp = requests.get('http://data.wake.opendata.arcgis.com/datasets/ebe3ae7f76954fad81411612d7c4fb17_1.geojson')
-        json_resp = resp.json()
-        if 'features' in json_resp:
-            return json_resp['features']
-        time.sleep(30)  # wake queues up the response
-        return self.get_restaurants()
+        return self.get_county_data(url)
 
     def save_restaurants(self, restaurants):
         for restaurant in restaurants:
